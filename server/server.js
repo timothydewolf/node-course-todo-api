34598d90650f572bd306ed70f7e20a57ea237419
Tim DeWolf <tim@tdewolf.net>
2018-09-18 11:18:41 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 
@@ -61,6 +62,26 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// url parameter
+app.get('/todos/:id', (req, res) => {
+    //res.send(req.params);
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
